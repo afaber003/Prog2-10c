eaa0865e25151de819487a0ddcbf99d36e92e3cd
afaber003 <afaber003@gmail.com>
2021-04-15 11:28:43 -0700

    Created Files and wrote input for file and setup

---
+++ WordLadder.cpp
@@ -0,0 +1,78 @@
+#include "WordLadder.h"
+
+
+WordLadder::WordLadder(const string& fileName) {
+    
+    //opens text file
+    ifstream dictionaryFile;
+    dictionaryFile.open(fileName);
+    if (!dictionaryFile.is_open()){
+        cout << "File failed to open" << endl;
+        return;
+    } 
+
+    //reading words to dict list
+    string temp = "";
+    while (dictionaryFile >> temp){
+        if (temp.size() != 5){
+            cout << temp << " is not 5 letters" << endl;
+            return;
+        }
+        dict.push_back(temp);
+    }
+    dictionaryFile.close();
+}
+
+
+
+
+
+void WordLadder::outputLadder(const string &start, const string &end, const string &outputFile){
+
+    //creating and opening answer file
+    ofstream answerFile;
+    answerFile.open("answerFile.txt");
+    if (!answerFile.is_open()){
+        cout << "Error Opening answerFile.txt" << endl;
+        return;
+    }
+
+    //checks for either word not existing in dictionary
+    bool found = false;
+
+        //checks starting word
+    for (list<string>::iterator i = dict.begin(); i != dict.end(); i++){
+        if (start == *i){
+            found == true;
+            break;
+        }
+    }
+    if (start == *dict.end()){
+        found == true;
+    }
+    if (found == false){
+        cout << "Starting word was not found in Dictionary" << endl;
+        return;
+    }
+    found == false;
+
+        //checks ending word
+    for (list<string>::iterator i = dict.begin(); i != dict.end(); i++){
+        if (end == *i){
+            found == true;
+            break;
+        }
+    }
+    if (start == *dict.end()){
+        found == true;
+    }
+    if (found == false){
+        cout << "Ending word was not found in Dictionary" << endl;
+        return;
+    }    
+
+    // Actual "Word-Ladder" Begins After This //
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "WordLadder.h"
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
